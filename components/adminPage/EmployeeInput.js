6986051eb8af2c34896abd05f105bb58774f71f1
Minira Samadova <samadova.minira@gmail.com>
2019-12-04 16:47:31 +0900

    added email validation and error alert to employee input

--- components/adminPage/EmployeeInput.js
+++ components/adminPage/EmployeeInput.js
@@ -1,3 +1,8 @@
+//util functions
+import { emailValidation } from '../../utils/utils';
+import SweetAlert from 'sweetalert-react';
+import 'sweetalert/dist/sweetalert.css';
+
 export default class EmployeeInput extends React.Component {
   constructor() {
     super();
@@ -7,17 +12,23 @@ export default class EmployeeInput extends React.Component {
       department: '',
       name: '',
       dateOfBirth: '',
-      type: ''
+      type: '',
+      isAlertShown: false
     };
   }
 
   handleInputChange = (e) => {
-    this.setState({ [e.target.name]: [e.target.value] });
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   handleFormSubmit = (e) => {
     e.preventDefault();
     //callback from parent which executes POST api call to the backend
+    // eslint-disable-next-line react/prop-types
+    if (!emailValidation(this.state.email)) {
+      this.setState({ isAlertShown: true });
+      return;
+    }
     this.props.addNewEmployee({ employeeId: this.state.employeeId, email: this.state.email, department: this.state.department, name: this.state.name, dateOfBirth: this.state.dateOfBirth, type: this.state.type });
   }
 
@@ -28,28 +39,28 @@ export default class EmployeeInput extends React.Component {
         <h3>Add employee invidually</h3>
         <form>
           <label>Employee ID</label>
-          <input name='employeeId' value={this.state.employeeId} onChange={this.handleInputChange}  />
+          <input id='employeeId' name='employeeId' value={this.state.employeeId} onChange={this.handleInputChange}  />
           <label>Email</label>
-          <input name='email' value={this.state.email} onChange={this.handleInputChange}  />
+          <input id='email' name='email' value={this.state.email} onChange={this.handleInputChange}  />
           <label>Department</label>
-          <input name='department' value={this.state.department} onChange={this.handleInputChange}  />
+          <input id='department' name='department' value={this.state.department} onChange={this.handleInputChange}  />
           <label>Name</label>
-          <input name='name' value={this.state.name} onChange={this.handleInputChange}  />
+          <input id='name' name='name' value={this.state.name} onChange={this.handleInputChange}  />
           <label>Date Of Birth</label>
-          <input name='dateOfBirth' value={this.state.dateOfBirth} onChange={this.handleInputChange}  />
+          <input id='dateOfBirth' name='dateOfBirth' value={this.state.dateOfBirth} onChange={this.handleInputChange}  />
           <label>Employee Type</label>
-          <input name='type' value={this.state.type} onChange={this.handleInputChange}  />
-          <button onClick={this.handleFormSubmit}>ADD</button>
+          <input id='type' name='type' value={this.state.type} onChange={this.handleInputChange}  />
+          <button onClick={this.handleFormSubmit}>ADD USER</button>
         </form>
+        <SweetAlert
+          show={this.state.isAlertShown}
+          title="Email Validation"
+          text="Please input a valid email!"
+          onConfirm={() => this.setState({ isAlertShown: false })}
+        />
         <h3>Bulk Upload</h3>
         <h4>csv file upload</h4>
       </div>
     );
   }
 }
-
-EmployeeInput.propTypes = {
-  addNewEmployee: PropTypes.node.isRequired
-};
-
-//need to invoke callback func from the parent component to pass info about added user
