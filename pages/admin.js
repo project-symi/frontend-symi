8ae469732331de133ecb7dac35254937eb6b2ac7
Minira Samadova <samadova.minira@gmail.com>
2019-12-04 12:22:24 +0900

    added admin page

---
+++ pages/admin.js
@@ -0,0 +1,21 @@
+/* eslint-disable react/no-unescaped-entities */
+import Layout from '../components/Layout';
+
+export default class Admin extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      addedUsers: null
+    };
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h1>Welcome to Symi!</h1>
+        <h3>Start to </h3>
+      </Layout>
+    );
+  }
+}
+
