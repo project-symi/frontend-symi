84d06af0d67dfad5bb81d80f27a27df7ef664b0d
steffieharrner <steffie.harner@gmail.com>
2019-12-04 17:18:31 +0900

    feedback about dropdown showing

--- components/employeePage/Feedback.js
+++ components/employeePage/Feedback.js
@@ -4,13 +4,42 @@ export default class Feedback extends React.Component {
     super();
     this.state = {
       feeling: null,
-      about: null,
-      notes: null
+      about: { type: null, input: null },
+      note: null
     };
   }
 
+  searchEmployee = event => {
+    this.setState({
+      about: {
+        type: this.state.about.type,
+        input: event.target.value
+      }
+    });
+  };
+
   handleInputChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+    console.log('event.target.name = ', event.target.name);
+    if (event.target.name === 'About') {
+      this.setState({
+        about: {
+          type: event.target.value,
+          input: null
+        }
+      });
+    } else if (
+      event.target.name === 'Employee' ||
+      event.target.name === 'Category'
+    ) {
+      this.setState({
+        about: {
+          type: this.state.about.type,
+          input: event.target.value
+        }
+      });
+    } else {
+      this.setState({ [event.target.name]: event.target.value });
+    }
   };
 
   handleSubmit = event => {
@@ -38,20 +67,42 @@ export default class Feedback extends React.Component {
 
         <div>
           <label>ABOUT </label>
-          <input
-            type="text"
-            name="about"
-            value={this.state.about}
-            onChange={this.handleInputChange}
-          ></input>
+          {/* 1st ABOUT SELECTION */}
+          <select name="About" onChange={this.handleInputChange}>
+            <option>--- select ---</option>
+            <option value="Employee">Employee</option>
+            <option value="Category">Category</option>
+          </select>
+
+          {/* 2nd ABOUT > EMPLOYEE SEARCH */}
+          {this.state.about.type === 'Employee' ? (
+            <input type="text" onChange={this.searchEmployee}></input>
+          ) : (
+            <div></div>
+          )}
+
+          {/* 2nd ABOUT > CATEGORY */}
+          {this.state.about.type === 'Category' ? (
+            <select name="Category" onChange={this.handleInputChange}>
+              <option>Work/Life Balance</option>
+              <option>Benefits</option>
+              <option>Holidays</option>
+              <option>Job Satisfaction</option>
+              <option>Company Policy</option>
+              <option>Other</option>
+            </select>
+          ) : (
+            <div></div>
+          )}
+          {/* CATEGORY SELECT */}
         </div>
 
         <div>
           <label>BECAUSE </label>
           <input
             type="text"
-            name="notes"
-            value={this.state.because}
+            name="note"
+            value={this.state.note}
             onChange={this.handleInputChange}
           ></input>
         </div>
@@ -63,7 +114,3 @@ export default class Feedback extends React.Component {
     );
   }
 }
-
-// Feedback.propTypes = {
-//   submitFeedback: PropTypes.node.isRequired
-// };
