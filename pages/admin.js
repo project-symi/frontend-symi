b9eb45cc753996c9d9634be2f1fad2c90a82789f
Minira Samadova <samadova.minira@gmail.com>
2019-12-04 12:32:25 +0900

    fixed adming page

--- pages/admin.js
+++ pages/admin.js
@@ -1,6 +1,10 @@
 /* eslint-disable react/no-unescaped-entities */
 import Layout from '../components/Layout';
 
+//components
+import UserInput from '../components/adminPage';
+
+
 export default class Admin extends React.Component {
   constructor() {
     super();
