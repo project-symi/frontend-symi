1e2cdb7809d6cc2786167b2d95dca15a4e1db0f1
Matthew Brooks <mdeanbrooks@gmail.com>
2019-12-28 23:36:36 +0900

    fix prettier config and add error, loading, permission handling for invite page

--- components/Invites.js
+++ components/Invites.js
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useEffect } from 'react';
 
 /////////////////MUI
 //import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
-import { Grid, Paper, Typography} from '@material-ui/core';
+import { Grid, Paper, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 ///////////////////Utils & Context API
@@ -22,7 +22,6 @@ import { Button } from '@material-ui/core';
 import swal from 'sweetalert';
 import '../assets/sweetalert.min.js';
 
-
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
@@ -32,7 +31,7 @@ const useStyles = makeStyles(theme => ({
     margin: 'auto',
     maxWidth: '100%',
     margin: '10px',
-    borderRadius: '12px'
+    borderRadius: '12px',
   },
   title: {
     fontWeight: 'bold',
@@ -41,24 +40,40 @@ const useStyles = makeStyles(theme => ({
     textShadow: '1.5px #3f50b5',
     fontFamily: 'Roboto Condensed',
     marginTop: '5px',
-  }
+  },
 }));
 
 const Invites = () => {
   const classes = useStyles();
   const ceoProps = useContext(CeoContext);
   const employeeProps = useContext(EmployeeContext);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  let props;
-  if (Object.keys(ceoProps).length > 0) {
-    props = ceoProps;
-    props.getAllInvitations();
-  }
-  if (Object.keys(employeeProps).length > 0) {
-    props = employeeProps;
-  }
+  useEffect(() => {
+    const init = async () => {
+      let props;
+      if (Object.keys(ceoProps).length > 0) {
+        props = ceoProps;
+        try {
+          await props.getAllInvitations();
+          setLoading(false);
+        } catch (e) {
+          setError(e.message);
+          setLoading(false);
+        }
+      }
+      if (Object.keys(employeeProps).length > 0) {
+        props = employeeProps;
+        setLoading(false);
+        setError("You don't have access to this data.");
+      }
+    };
 
-  const handleInvitation = (invitation) => {
+    init();
+  }, []);
+
+  const handleInvitation = invitation => {
     swal({
       title: 'Can you make the meeting?',
       text: 'Please either accept or decline.',
@@ -66,135 +81,207 @@ const Invites = () => {
         element: 'input',
         attributes: {
           placeholder: 'Send a note back.',
-        }
+        },
       },
       icon: 'warning',
       buttons: {
         confirm: {
           text: 'ACCEPT',
-          value: true
+          value: true,
         },
-        cancel: 'DECLINE'
-      }
+        cancel: 'DECLINE',
+      },
     }).then(value => {
       if (value) {
         swal({
           title: 'Lunch invitation accepted!',
           icon: 'success',
-          button: true
+          button: true,
         }).then(() => {
           const answer = {
             status: true,
-            reply: value
+            reply: value,
           };
           props.handleInvitation(invitation, answer);
         });
       } else {
         const answer = {
           status: false,
-          reply: swal.getState().actions.confirm.value
+          reply: swal.getState().actions.confirm.value,
         };
         props.handleInvitation(invitation, answer);
       }
     });
   };
 
+  if (loading) {
+    return (
+      <div>
+        <p className="title">Invites</p>
+        <div className="data-big">
+          <img src={Loader}></img>
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p className="title">Invites</p>
+        <div className="data-big">{error}</div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <p className="title">Invites</p>
-      {
-        props.invitations ?
-          <div>
-            <div className={classes.root}>
-            </div>
-            {props.invitations.map((item) => { if (item.status == 'pending') {
+      {props.invitations ? (
+        <div>
+          <div className={classes.root}></div>
+          {props.invitations.map(item => {
+            if (item.status == 'pending') {
               return (
-                <Paper key={item.invitationId} className={classes.paper}>
+                <Paper
+                  key={item.invitationId}
+                  className={classes.paper}
+                >
                   <Grid container spacing={2}>
                     <Grid item xs={12} sm container>
-                      <Grid item xs container direction="column" spacing={2}>
+                      <Grid
+                        item
+                        xs
+                        container
+                        direction="column"
+                        spacing={2}
+                      >
                         <Grid item xs>
-                          <span className="date">{moment(item.invitationDate).format('MMMM Do YYYY').toUpperCase()} AT {item.invitationTime.toUpperCase()}</span>
+                          <span className="date">
+                            {moment(item.invitationDate)
+                              .format('MMMM Do YYYY')
+                              .toUpperCase()}{' '}
+                            AT {item.invitationTime.toUpperCase()}
+                          </span>
 
-                          <Typography className={classes.title} gutterBottom variant="subtitle1">
-                             Meeting with {item.employeeName ? item.employeeName : 'CEO'}
+                          <Typography
+                            className={classes.title}
+                            gutterBottom
+                            variant="subtitle1"
+                          >
+                            Meeting with{' '}
+                            {item.employeeName
+                              ? item.employeeName
+                              : 'CEO'}
                           </Typography>
-                          <p>
-                            {item.comments}
-                          </p>
+                          <p>{item.comments}</p>
                         </Grid>
                       </Grid>
                       <Grid item>
-                        {
-                          item.employeeName ? <p>{item.status === 'accepted' ? (
-                            <div className="status">
-                              <CheckCircleOutlineIcon style={{ color: 'green' }} />
-                              <div>accepted</div>
-                            </div>
-                          ) : (
-                            <div className="status">
-                              <HelpOutlineIcon style={{ color: 'purple' }} />
-                              <div>pending</div>
-                            </div>
-                          )}</p> : <p>
-                            { item.status === 'pending' ?
-                              <Button onClick={() => handleInvitation(item)} style={{ cursor: 'pointer' }} color="primary" variant="contained">Answer</Button>
-                              : null}
+                        {item.employeeName ? (
+                          <p>
+                            {item.status === 'accepted' ? (
+                              <div className="status">
+                                <CheckCircleOutlineIcon
+                                  style={{ color: 'green' }}
+                                />
+                                <div>accepted</div>
+                              </div>
+                            ) : (
+                              <div className="status">
+                                <HelpOutlineIcon
+                                  style={{ color: 'purple' }}
+                                />
+                                <div>pending</div>
+                              </div>
+                            )}
                           </p>
-                        }
-                      
+                        ) : (
+                          <p>
+                            {item.status === 'pending' ? (
+                              <Button
+                                onClick={() => handleInvitation(item)}
+                                style={{ cursor: 'pointer' }}
+                                color="primary"
+                                variant="contained"
+                              >
+                                Answer
+                              </Button>
+                            ) : null}
+                          </p>
+                        )}
                       </Grid>
                     </Grid>
                   </Grid>
                 </Paper>
               );
             }
-            })}</div> : 
-          <div className="data-big">
-            <img src={Loader}></img>
-          </div>
-      }
- 
-      {
-        props.invitations ?
-          <div>
-            <div className={classes.root}>
-            </div>
-            {props.invitations.map((item) => { if (item.status == 'accepted') {
+          })}
+        </div>
+      ) : (
+        <div className="data-big">
+          <img src={Loader}></img>
+        </div>
+      )}
+
+      {props.invitations ? (
+        <div>
+          <div className={classes.root}></div>
+          {props.invitations.map(item => {
+            if (item.status == 'accepted') {
               return (
-                <Paper key={item.invitationId} className={classes.paper}>
+                <Paper
+                  key={item.invitationId}
+                  className={classes.paper}
+                >
                   <Grid container spacing={2}>
                     <Grid item xs={12} sm container>
-                      <Grid item xs container direction="column" spacing={2}>
+                      <Grid
+                        item
+                        xs
+                        container
+                        direction="column"
+                        spacing={2}
+                      >
                         <Grid item xs>
-                          <span className="date">{moment(item.invitationDate).format('MMMM Do YYYY').toUpperCase()} AT {item.invitationTime.toUpperCase()}</span>
+                          <span className="date">
+                            {moment(item.invitationDate)
+                              .format('MMMM Do YYYY')
+                              .toUpperCase()}{' '}
+                            AT {item.invitationTime.toUpperCase()}
+                          </span>
 
-                          <Typography className={classes.title} gutterBottom variant="subtitle1">
-                             Meeting with {item.employeeName ? item.employeeName : 'CEO'}
+                          <Typography
+                            className={classes.title}
+                            gutterBottom
+                            variant="subtitle1"
+                          >
+                            Meeting with{' '}
+                            {item.employeeName
+                              ? item.employeeName
+                              : 'CEO'}
                           </Typography>
-                          <p>
-                            {item.comments} 
-                          </p>
+                          <p>{item.comments}</p>
                         </Grid>
                       </Grid>
                       <Grid item>
                         {item.status === 'accepted' ? (
                           <div className="status">
-                            <CheckCircleOutlineIcon style={{ color: 'green' }} />
+                            <CheckCircleOutlineIcon
+                              style={{ color: 'green' }}
+                            />
                             <div>accepted</div>
                           </div>
-                        ) : null }
-                      
+                        ) : null}
                       </Grid>
                     </Grid>
                   </Grid>
                 </Paper>
               );
             }
-            })}</div> : 
-          null
-      }
-
+          })}
+        </div>
+      ) : null}
     </div>
   );
 };
